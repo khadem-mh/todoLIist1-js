214edba3dc7c793481431cfeb7723a8dd3dc1441
khadem-mh <khadem13359@gmail.com>
2024-04-05 12:38:58 +0330

    dynamic date

--- Js/app.js
+++ Js/app.js
@@ -1,2 +1,5 @@
 const dateMonth = document.querySelector('.month')
 const dateToday = document.querySelector('.today') 
+const date = new Date()
+dateToday.innerHTML = date.getDay()
+dateMonth.innerHTML = date.getMonth() + 1
