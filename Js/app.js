c8a20267472a71ea19145379e0935d0707b3c704
khadem-mh <khadem13359@gmail.com>
2024-04-07 16:39:05 +0330

    select btnSetNewNameList

--- Js/app.js
+++ Js/app.js
@@ -2,15 +2,21 @@ import monthNames from "./monthNames.js"
 //Elements
 const addNewList = document.querySelector('#addNewList')
 const inpNewListName = document.querySelector('#inpNewListName').value
+const btnSetNewNameList = document.querySelector('#btnSetNewNameList').value
 const dateMonth = document.querySelector('.month')
-const dateToday = document.querySelector('.today') 
+const dateToday = document.querySelector('.today')
 
 const addNewListHandler = () => {
 
 }
 
+const btnSetNewNameListHandler = () => {
+
+}
+
 const date = new Date()
 dateToday.innerHTML = date.getDay()
-dateMonth.innerHTML = monthNames[date.getMonth()] 
+dateMonth.innerHTML = monthNames[date.getMonth()]
 
 addNewList.addEventListener('click', addNewListHandler)
+btnSetNewNameList.addEventListener('click', btnSetNewNameListHandler)
