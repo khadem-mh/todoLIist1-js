76b96fd22bf4f9c3a1c5c0883fec55d8e4f35bb2
khadem-mh <khadem13359@gmail.com>
2024-04-07 16:45:10 +0330

    coding logic for visible modal

--- Js/app.js
+++ Js/app.js
@@ -2,12 +2,18 @@ import monthNames from "./monthNames.js"
 //Elements
 const addNewList = document.querySelector('#addNewList')
 const inpNewListName = document.querySelector('#inpNewListName').value
-const btnSetNewNameList = document.querySelector('#btnSetNewNameList').value
+const btnSetNewNameList = document.querySelector('#btnSetNewNameList')
+const parentModal = document.querySelector('#parentModal')
 const dateMonth = document.querySelector('.month')
 const dateToday = document.querySelector('.today')
 
 const addNewListHandler = () => {
+    if (condition) {
+        parentModal.classList.remove('modal-hide')
+        parentModal.classList.add('modal-visible')
+    } else {
 
+    }
 }
 
 const btnSetNewNameListHandler = () => {
