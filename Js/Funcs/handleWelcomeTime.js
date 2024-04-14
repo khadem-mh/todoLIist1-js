60dc848b52bd125e87caf3caf7e48f40427e42de
khadem-mh <khadem13359@gmail.com>
2024-04-14 08:33:17 +0330

    coding logic handle welcome time end

--- Js/Funcs/handleWelcomeTime.js
+++ Js/Funcs/handleWelcomeTime.js
@@ -1,7 +1,10 @@
 const handleWelcomeTime = hours => {
     if (typeof hours === "number") {
-        
-    } else {
-        return console.error('please put number')
-    }
+        switch (hours) {
+            case hours >= 0 && hours <= 11: return "Good Morning"
+            case hours >= 11 && hours <= 16: return "Good Afternoon"
+            case hours >= 16 && hours <= 19: return "Good Evening"
+            case hours >= 19 && hours <= 0: return "Good Night"           
+        }
+    } else return console.error('please put number')
 }
